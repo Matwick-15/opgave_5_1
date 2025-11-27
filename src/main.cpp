aca719da9214105faf4ce223931f221b9982f4b1
Dánjal Leitisstein Olsen <danjal.olsen04@gmail.com>
2025-11-27 11:57:17 +0100

    Implemented quadrature frequency

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,14 @@
 #define GEAR_RATIO 30.0
 
 // Timer flags for the Timer A0 ISR.
-volatile char t_flag1 = 0, t_flag2 = 0;
+volatile char t_flag = 0, t_flag2 = 0;
 
 // Variables to store the captured values from the motor encoders.
-unsigned int captured_value1 = 0;
-unsigned int captured_value2 = 0;
+unsigned int captured_value = 0;
 
 // freqx is the raw frequency from the motor encoders.
 // The actual rotational frequency is freqx / SCALER.
-float freq1 = 0, freq2 = 0;
+float freq = 0;
 // float freqMax = 525.0;
 
 // Function to set up the OLED-display.
@@ -138,14 +137,11 @@ int main() {
   float RPS = 0;
   float RPM = 0;
   float shaft_RPM = 0;
-  unsigned int counter1 = 0;
-  unsigned int counter2 = 0;
+  unsigned int counter = 0;
 
   // Variables necessary for averaging frequencies.
-  float freq1_av = 0;
-  float freq2_av = 0;
+  float freq_av = 0;
   unsigned int i = 0;
-  unsigned int n = 0;
 
   // Buffers etc. for printing.
   char duty_cycle_buffer[32] = {};
@@ -164,27 +160,27 @@ int main() {
     sprintf(duty_cycle_buffer, "Duty cycle: %s%%", temp_buffer);
     ssd1306_printText(0, 0, duty_cycle_buffer);
 
-    if (t_flag1) {
-      t_flag1 = 0;
-      counter1++;
+    if (t_flag) {
+      t_flag = 0;
+      counter++;
 
-      freq1_av += freq1;
+      freq_av += freq;
       i++;
 
       if (i >= 10) {
-        freq1 = freq1_av / 10.0;
-        freq1_av = 0.0;
+        freq = freq_av / 10.0;
+        freq_av = 0.0;
         i = 0;
 
         // The RPS is the raw frequency divided by # of pulses per revolution.
-        RPS = freq1 / SCALER;
+        RPS = freq / SCALER;
 
-        if (counter1 >= 100) {
-          counter1 = 0;
+        if (counter >= 100) {
+          counter = 0;
 
           // Print the raw frequency from moter encoder 1.
-          dtostrf(freq1, 0, 2, temp_buffer);
-          sprintf(freq_buffer, "Freq1: %sHz    ", temp_buffer);
+          dtostrf(freq, 0, 2, temp_buffer);
+          sprintf(freq_buffer, "Freq: %sHz    ", temp_buffer);
           ssd1306_printText(0, 1, freq_buffer);
 
           // Print the RPS of the motor.
@@ -204,37 +200,6 @@ int main() {
         }
       }
     }
-
-    if (t_flag2) {
-      t_flag2 = 0;
-      counter2++;
-
-      freq2_av += freq2;
-      n++;
-
-      if (n >= 10) {
-        freq2 = freq2_av / 10.0;
-        freq2_av = 0.0;
-        n = 0;
-
-        // The RPS is the raw frequency divided by # of pulses per revolution.
-        RPS = freq2 / SCALER;
-
-        if (counter2 >= 100) {
-          counter2 = 0;
-
-          // Print the raw frequency from moter encoder 1.
-          dtostrf(freq2, 0, 2, temp_buffer);
-          sprintf(freq_buffer, "Freq2: %sHz    ", temp_buffer);
-          ssd1306_printText(0, 2, freq_buffer);
-
-          // Print the RPS of the motor.
-          dtostrf(RPS, 0, 2, temp_buffer);
-          sprintf(RPS_buffer, "RPS: %s    ", temp_buffer);
-          ssd1306_printText(0, 3, RPS_buffer);
-        }
-      }
-    }
   }
 }
 
@@ -242,56 +207,37 @@ int main() {
 #pragma vector = TIMER0_A1_VECTOR
 __interrupt void Timer_A0_ISR(void) {
   // Variables to store
-  static unsigned int last1 = 0, last2 = 0;
+  static unsigned int last = 0;
   static int i = 0, n = 0;
 
   switch (TA0IV) {
   case 0x02: // Interrupt caused by CCR1 = P1.2.
              // Handle the captured value for the first encoder pulse.
-    // Handle overflow if last1 is greater than TA0CCR1.
-    if (last1 > TA0CCR1) {
-      captured_value1 = 65535 - last1 + TA0CCR1;
+    // Handle overflow if last is greater than TA0CCR1.
+    if (last > TA0CCR1) {
+      captured_value = 65535 - last + TA0CCR1;
     } else {
-      captured_value1 = (TA0CCR1 - last1);
+      captured_value = (TA0CCR1 - last);
     }
-    last1 = TA0CCR1;
+    last = TA0CCR1;
     i++;
     // Only calculate the frequency every other encoder pulse.
     if (i >= 2) {
-      if (captured_value1 == 0) {
-        captured_value1 = 1;
+      if (captured_value <= 0) {
+        captured_value = 1;
       }
 
-      freq1 = (float)(32768.0 / captured_value1);
+      freq = (float)(32768.0 / captured_value);
 
-      captured_value1 = 0;
+      captured_value = 0;
       i = 0;
-      t_flag1 = 1;
+      t_flag = 1;
     }
     break;
 
   case 0x04: // Interrupt caused by CCR2 = P1.3.
              // Handle the captured value for the second encoder pulse.
-    // Handle overflow if last2 is greater than TA0CCR2.
-    if (last2 > TA0CCR2) {
-      captured_value2 = 65535 - last2 + TA0CCR2;
-    } else {
-      captured_value2 = (TA0CCR2 - last2);
-    }
-    last2 = TA0CCR2;
-    n++;
-    // Only calculate the frequency every other encoder pulse.
-    if (n >= 2) {
-      if (captured_value2 == 0) {
-        captured_value2 = 1;
-      }
-
-      freq2 = (float)(32768.0 / captured_value2);
-
-      captured_value2 = 0;
-      n = 0;
-      t_flag2 = 1;
-    }
+    last = TA0CCR2;
     break;
 
   default:
